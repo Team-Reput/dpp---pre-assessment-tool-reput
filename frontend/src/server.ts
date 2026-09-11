659fb2524e54f5bcaa4bd3249fa36a91fa807142
Jyoti0101-reput <jyoti@reput.co.in>
2026-09-11 12:06:05 +0530

    nginx file addded

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -6,27 +6,27 @@ import {
 } from '@angular/ssr/node';
 import express from 'express';
 import { join } from 'node:path';
-
+ 
 const browserDistFolder = join(import.meta.dirname, '../browser');
-
+ 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
-
+ 
 /**
- * Example Express Rest API endpoints can be defined here.
- * Uncomment and define endpoints as necessary.
- *
- * Example:
- * ```ts
- * app.get('/api/{*splat}', (req, res) => {
- *   // Handle API request
- * });
- * ```
- */
-
+* Example Express Rest API endpoints can be defined here.
+* Uncomment and define endpoints as necessary.
+*
+* Example:
+* ```ts
+* app.get('/api/{*splat}', (req, res) => {
+*   // Handle API request
+* });
+* ```
+*/
+ 
 /**
- * Serve static files from /browser
- */
+* Serve static files from /browser
+*/
 app.use(
   express.static(browserDistFolder, {
     maxAge: '1y',
@@ -34,10 +34,10 @@ app.use(
     redirect: false,
   }),
 );
-
+ 
 /**
- * Handle all other requests by rendering the Angular application.
- */
+* Handle all other requests by rendering the Angular application.
+*/
 app.use((req, res, next) => {
   angularApp
     .handle(req)
@@ -46,23 +46,23 @@ app.use((req, res, next) => {
     )
     .catch(next);
 });
-
+ 
 /**
- * Start the server if this module is the main entry point, or it is ran via PM2.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
- */
+* Start the server if this module is the main entry point, or it is ran via PM2.
+* The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+*/
 if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
+  const port = Number(process.env['PORT']) || 4000;
+  app.listen(port, '0.0.0.0', (error) => {
     if (error) {
       throw error;
     }
-
+ 
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
-
+ 
 /**
- * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
- */
+* Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
+*/
 export const reqHandler = createNodeRequestHandler(app);
